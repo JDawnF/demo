6f5d2c62e37a1c52b8b00014c306fb3be341ebea
JDawnF <335825732@qq.com>
2019-09-01 19:33:15 +0800

    自动装配demo

---
+++ geektime-spring-family-master/Chapter 9/geektime-spring-boot-autoconfigure/src/main/java/geektime/spring/hello/greeting/GreetingAutoConfiguration.java
@@ -0,0 +1,19 @@
+package geektime.spring.hello.greeting;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(GreetingApplicationRunner.class)
+public class GreetingAutoConfiguration {
+    @Bean
+    @ConditionalOnMissingBean(GreetingApplicationRunner.class)//不存在GreetingApplicationRunner时才生效
+    // matchIfMissing表示没配置@ConditionalOnProperty时也生效
+    @ConditionalOnProperty(name = "greeting.enabled", havingValue = "true", matchIfMissing = true)
+    public GreetingApplicationRunner greetingApplicationRunner() {
+        return new GreetingApplicationRunner();
+    }
+}
